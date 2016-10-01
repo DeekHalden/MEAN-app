c8991352ff058244dd1fe3d0443692ba1b78aa05
deekhalden <iliy4@ua.fm>
2016-10-01 19:22:32 +0300

    better

--- app/routes/verify.js
+++ app/routes/verify.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const User = require('../models/user'),
+var User = require('../models/user'),
  jwt = require('jsonwebtoken')
 
 
@@ -11,22 +11,22 @@ exports.getToken = (user) =>{
 
 exports.verifyOrdinaryUser = (req, res, next) =>{
     // check header or params
-    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
     if (token) {
         jwt.verify(token, process.env.secretKey, (err, decoded) =>{
             if (err) {
-                const err = new Error('You are not authenticated!');
+                var err = new Error('You are not authenticated!');
                 err.status = 401;
                 return next(err);
             } else {
                 req.decoded = decoded;
-                const flag = req.decoded.admin;
+                var flag = req.decoded.admin;
                 next();
             }
         });
     } else {
-        const err = new Error('No token provided!');
+        var err = new Error('No token provided!');
         err.status = 403;
         return next(err);
     }
@@ -36,14 +36,14 @@ exports.verifyOrdinaryUser = (req, res, next) =>{
 
 exports.verifyAdmin = (req, res, next) =>{
     if (!req.decoded) {
-        const err = new Error('Not authorized')
+        var err = new Error('Not authorized')
         err.status = 403;
         return next(err);
 
     } else {
-        const id = req.decoded._id;
+        var id = req.decoded._id;
         if (!req.decoded.admin) {
-            const err = new Error('Not authorized')
+            var err = new Error('Not authorized')
             err.status = 403;
             return next(err);
         }else {
